e2b77cde99104fc415cda5a27f25f96be93f8bd3
Bjorn <>
2019-05-06 10:51:58 +0200

    safe sequence

---
+++ src/block2/cp/tests/SafeSequence.java
@@ -0,0 +1,18 @@
+package block2.cp.tests;
+
+import net.jcip.annotations.NotThreadSafe;
+
+@NotThreadSafe
+public class SafeSequence {
+    private int value;
+
+    public int getNext() {
+        return value++;
+    }
+
+    public void doFor(int x) {
+        for(int i = 0; i < x; i++) {
+            getNext();
+        }
+    }
+}
---
+++ src/block2/cp/tests/UnsafeSequenceTest.java
@@ -0,0 +1,33 @@
+package block2.cp.tests;
+
+import nl.utwente.pp.cp.junit.ConcurrentRunner;
+import nl.utwente.pp.cp.junit.Threaded;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcurrentRunner.class)
+public class UnsafeSequenceTest {
+    private static final int THREAD_COUNT = 4;
+    private  static final int DO_TIMES = 100;
+
+    private final UnsafeSequence us = new UnsafeSequence();
+
+    @Test
+    @Threaded(count = THREAD_COUNT)
+    public void doAdd() {
+//        for (int i = 0; i < DO_TIMES; i++) {
+//            System.out.println(us.getNext());
+//        }
+        us.doFor(DO_TIMES);
+    }
+
+    @After
+    public void afterTest() {
+        int x = us.getNext();
+        System.out.println(x);
+        assert(x == (THREAD_COUNT * DO_TIMES));
+    }
+
+
+}
