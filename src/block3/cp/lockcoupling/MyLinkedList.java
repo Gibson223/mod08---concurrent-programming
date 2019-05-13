fe94cd815be74f2fdb7f4e20175e713e283ea311
Bjorn <>
2019-05-13 12:29:40 +0200

    not fully working exercise 3

--- src/block3/cp/lockcoupling/MyLinkedList.java
+++ src/block3/cp/lockcoupling/MyLinkedList.java
@@ -1,9 +1,11 @@
 package block3.cp.lockcoupling;
 
+import block2.cp.queue.LinkedNode;
+
 public class MyLinkedList implements List {
 
     private int size;
-    private MyLinkedNode firstNode;
+    private MyLinkedNode firstNode = new MyLinkedNode(null);
 
     /**
      * Insert an element at the specified position in the list.
@@ -12,7 +14,38 @@ public class MyLinkedList implements List {
      * @param value    The value of the element to insert.
      */
     @Override
-    public void insert(int position, Object value) {
+    public void insert(int position, Object value) throws IndexOutOfBoundsException {
+        firstNode.lock.lock();
+        int currentIndex = 0;
+        try {
+            if (firstNode.getThisObject() == null) {
+                firstNode.setThisObject(value);
+            } else {
+                MyLinkedNode currentNode = firstNode;
+                MyLinkedNode nextNode;
+                try {
+                    while (currentNode.hasNext() && position > currentIndex) {
+                        try {
+                            nextNode = currentNode.getNext();
+                            nextNode.lock.lock();
+                            currentIndex++;
+                        } finally {
+                            currentNode.lock.unlock();
+                        }
+                        currentNode = nextNode;
+                    }
+                    if (currentNode.hasNext()) {
+                        throw new IndexOutOfBoundsException("Index is out of bounds");
+                    }
+                    MyLinkedNode newNode = new MyLinkedNode(value);
+                    currentNode.setNext(newNode);
+                } finally {
+                    currentNode.lock.unlock();
+                }
+            }
+        } finally {
+            firstNode.lock.unlock();
+        }
     }
 
     /**
@@ -22,7 +55,32 @@ public class MyLinkedList implements List {
      */
     @Override
     public void add(Object value) {
-
+        firstNode.lock.lock();
+        try {
+            if (firstNode.getThisObject() == null) {
+                firstNode.setThisObject(value);
+            } else {
+                MyLinkedNode currentNode = firstNode;
+                MyLinkedNode nextNode;
+                try {
+                    while (currentNode.hasNext()) {
+                        try {
+                            nextNode = currentNode.getNext();
+                            nextNode.lock.lock();
+                        } finally {
+                            currentNode.lock.unlock();
+                        }
+                        currentNode = nextNode;
+                    }
+                    MyLinkedNode newNode = new MyLinkedNode(value);
+                    currentNode.setNext(newNode);
+                } finally {
+                    currentNode.lock.unlock();
+                }
+            }
+        } finally {
+            firstNode.lock.unlock();
+        }
     }
 
     /**
@@ -42,7 +100,38 @@ public class MyLinkedList implements List {
      */
     @Override
     public void delete(int position) {
-
+        int currentIndex = 0;
+        firstNode.lock.lock();
+        try {
+            if (firstNode.getThisObject() != null) {
+                MyLinkedNode prevNode = firstNode;
+                MyLinkedNode currentNode;
+                try {
+                    if (!prevNode.hasNext()) {
+                        prevNode.setThisObject(null);
+                    } else {
+                        currentNode = prevNode.getNext();
+                        currentNode.lock.lock();
+                        try {
+                            while (currentNode.hasNext() && ) {
+                                prevNode.lock.unlock();
+                                prevNode = currentNode;
+                                currentNode = prevNode.getNext();
+                                currentNode.lock.lock();
+                            }
+                            prevNode.setNext(null);
+                        } finally {
+                            currentNode.lock.unlock();
+                            prevNode.lock.unlock();
+                        }
+                    }
+                } finally {
+                    prevNode.lock.unlock();
+                }
+            }
+        } finally {
+            firstNode.lock.unlock();
+        }
     }
 
     /**
--- src/block3/cp/lockcoupling/MyLinkedNode.java
+++ src/block3/cp/lockcoupling/MyLinkedNode.java
@@ -1,7 +1,11 @@
 package block3.cp.lockcoupling;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class MyLinkedNode {
 
+    public final Lock lock = new ReentrantLock();
     private Object thisObject;
     private MyLinkedNode next;
 
@@ -15,6 +19,14 @@ public class MyLinkedNode {
 
     public void setNext(MyLinkedNode next) {
         this.next = next;
+    }
+
+    public void setThisObject(Object thisObject) {
+        this.thisObject = thisObject;
+    }
+
+    public Boolean hasNext() {
+        return next != null;
     }
 
     public Object getThisObject() {
