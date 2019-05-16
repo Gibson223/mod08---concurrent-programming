4a762445154bc34e3b384cdf2bc161dd25a49df4
Bjorn <>
2019-05-16 12:27:24 +0200

    Added tests for exercise 5

--- src/block3/cp/Exercise5/Runner.java
+++ src/block3/cp/Exercise5/Runner.java
@@ -1,5 +1,7 @@
 package block3.cp.Exercise5;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 
 public class Runner {
@@ -7,12 +9,39 @@ public class Runner {
 	public static void main(String[] args) {
 		ArrayList<Integer> arrlist = new ArrayList<>();
 		arrlist.add(1);
-		arrlist.add(1);
+		arrlist.add(2);
+		arrlist.add(3);
+		arrlist.add(4);
+		arrlist.add(5);
+		NewerMapReduceBase mrb = new NewerMapReduceBase(arrlist);
+		mrb.run();
+		System.out.println(mrb.getOutput());
+	}
+
+	@Test
+	public void doTest1() {
+		ArrayList<Integer> arrlist = new ArrayList<>();
 		arrlist.add(1);
 		arrlist.add(1);
 		arrlist.add(1);
 		NewerMapReduceBase mrb = new NewerMapReduceBase(arrlist);
 		mrb.run();
-		System.out.println(mrb.getOutput());
+		assert (mrb.getOutput() == 6);
+		assert (arrlist.size() == 3);
 	}
+
+	@Test
+	public void doTest2() {
+		ArrayList<Integer> arrlist = new ArrayList<>();
+		arrlist.add(1);
+		arrlist.add(2);
+		arrlist.add(3);
+		arrlist.add(4);
+		arrlist.add(5);
+		NewerMapReduceBase mrb = new NewerMapReduceBase(arrlist);
+		mrb.run();
+		assert (mrb.getOutput() == 20);
+		assert (arrlist.size() == 5);
+	}
+
 }
