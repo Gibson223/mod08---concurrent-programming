5652e756f43f1c5ee09dc768188f1f38da5db3cc
Gibson <g.w.r.vredeveld@student.utwente.nl>
2019-05-17 14:23:18 +0200

    lalalal

---
+++ src/block4/cp/LeftRightlock.java
@@ -0,0 +1,13 @@
+package block4.cp;
+
+public class LeftRightDeadlock {
+    private final Object left = new Object();
+    private final Object right = new Object();
+    public void leftRight() {
+        synchronized (left) {
+            synchronized (right) {
+                doSomething();            }        }    }
+    public void rightLeft() {
+        synchronized (right) {
+            synchronized (left) {
+                doSomethingElse();            }        }    } }
